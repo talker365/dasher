da32dd9e7ca0cdcd0d95af5dc2dcb12e959bb650
N4LDR <loren@ristolaonline.com>
2024-02-03 01:39:25 -0500

    Updated Scripts
    Corrected stats
    
    Make sure to have newest rsync on mac  Brew install rsync or brew upgrade rsync

--- pullForGit.sh
+++ pullForGit.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-rsync -a --info=progress2 /Volumes/sigmon/var/www/html/ src/var/www/html
+start=$(date +'%s')
+echo -e '\nTransfering files...'
+rsync -ar --info=progress2 /Volumes/sigmon/var/www/html/ src/var/www/html
+echo -e "\n\nFile Transfer Completed! in $(($(date +'%s') - $start)) seconds."
--- pushFromGit.sh
+++ pushFromGit.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
+start=$(date +'%s')
+echo -e '\nTransfering files...'
 rsync -ar --info=progress2 src/var/www/html/ /Volumes/sigmon/var/www/html 
+echo -e "\n\nFile Transfer Completed! in $(($(date +'%s') - $start)) seconds."
