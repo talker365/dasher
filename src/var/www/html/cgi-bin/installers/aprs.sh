841d5d6a30cb19b5d4a52c34f095e82b4531a0bb
N4LDR <loren@ristolaonline.com>
2024-02-26 11:53:53 -0500

    Update aprs.sh

--- src/var/www/html/cgi-bin/installers/aprs.sh
+++ src/var/www/html/cgi-bin/installers/aprs.sh
@@ -43,22 +43,11 @@ while [[ "$#" -gt 0 ]]; do
   shift
 done
 
-# Function to verify callsign format
-verify_callsign() {
-    local callsign=$(echo "$CALLSIGN" | tr '[:lower:]' '[:upper:]')  # Convert to uppercase
-    local pattern="^[A-Z]{1,2}[0-9][A-Z]{1,3}$"
-    if [[ $callsign =~ $pattern && ${#callsign} -le 6 ]]; then
-        echo "$CALLSIGN is a valid Amateur Radio Callsign."
-    else
-        echo "$CALLSIGN is not a valid Amateur Radio Callsign."
-        exit 1
-    fi
-}
-
 # Function to check if a command is installed
 check_command() {
   if ! command -v "$1" &> /dev/null; then
     echo "$1 is not installed or not executable"
+    echo -e "\nDasher Status: Failure"
     exit 1
   fi
 }
@@ -66,18 +55,18 @@ check_command() {
 # Function to uninstall pyPacket and its dependents
 uninstall_pypacket() {
   sudo rm -rf "$INSTALL_DIR"/pypacket
+  sudo rm -rf "$INSTALL_DIR"/bin/pypacket
+  sudo rm -rf "$INSTALL_DIR"/share/pypacket
   sudo rm -rf /etc/systemd/system/pyPacket.service
   sudo rm -rf /etc/profile.d/02-pypacket.sh
   echo "pyPacket and its dependents have been uninstalled."
 }
-
 
 # If uninstall flag is set, execute uninstall function
 if [ "$uninstall" = true ]; then
   uninstall_pypacket
   exit 0
 fi
-
 
 # Check if mandatory options are provided
 missing_options=()
@@ -95,13 +84,9 @@ fi
 
 if [[ ${#missing_options[@]} -gt 0 ]]; then
   echo "Error: Mandatory options ${missing_options[@]} are required."
+  echo -e "\nDasher Status: Failure"
   exit 1
 fi
-
-
-# Otherwise, install pyPacket and its dependents
-verify_callsign "$CALLSIGN"
-
 
 # Check if required commands are installed
 check_command "pip3"
@@ -110,22 +95,26 @@ check_command "figlet"
 check_command "multimon-ng"
 
 # Update package list
+echo "Updating APT Packages"
 sudo apt update
 
 # Clone pyPacket repository
-cd "$INSTALL_DIR" || exit 1
+cd "$INSTALL_DIR" 
 echo "Cloning pyPacket repository..."
 git clone https://github.com/talker365/pypacket.git
-cd pypacket || exit 1
+cd pypacket 
 
 # Edit requirements.txt file and change pytest version
-sed -i 's/^pytest==.*/pytest==4.6.9/' requirements.txt
+echo "Editing the requirements.txt file to change pytest version."
 pip3 install -r requirements.txt
+sed -i 's/^pytest==.*/pytest==4.6.9/' requirements.txt
 
 # Update configuration with serial number
+echo "Updating the configuration.json file with the provided $SERIAL_NUMBER serial number."
 sed -i 's/"serial": "12345678"/"serial": "'"$SERIAL_NUMBER"'"/' config/configuration.json
 
 # Update rtl_listener.py with serial number
+echo "Updating rtl_listener.py file."
 sed -i '12s/.*/config.sample_rate(), "-l", "0", "-d", config.serial(), "-g", config.gain(), "-"],/' implementations/rtl_listener.py
 
 # Create the pyPacket profile file
@@ -141,7 +130,7 @@ cat << EOF | sudo tee /opt/pypacket/start.sh > /dev/null
 #!/bin/bash
 export PYPACKET_USERNAME="$CALLSIGN"
 export PYPACKET_PASSWORD="$PASSCODE"
-cd /opt/pypacket/ || exit 1
+cd /opt/pypacket/
 python3 main.py
 EOF
 
@@ -171,4 +160,4 @@ echo "Enabling and starting the pyPacket service..."
 sudo systemctl enable pyPacket
 sudo systemctl start pyPacket
 
-echo "APRS / pyPacket and its dependents have been installed successfully."
+echo -e "\nDasher Status: Success"
